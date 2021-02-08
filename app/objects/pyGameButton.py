05c34447bdb9c82b3de6421c3680397af18eadeb
Ivanm <ivanm2114@mail.ru>
2021-02-08 17:32:01 +0300

    Button update 4 -button work properly

--- app/objects/pyGameButton.py
+++ app/objects/pyGameButton.py
@@ -2,7 +2,9 @@ import pygame
 
 
 class pyButton:
-    def __init__(self, x, y, text, screen):
+    def __init__(self, x, y, text, screen,func,arr):
+        arr.append(self)
+        self.func = func
         self.x = x
         self.y = y
         self.text = text
@@ -11,17 +13,21 @@ class pyButton:
         self.string = self.font.render(self.text, True, pygame.Color(255, 255, 255))
         self.intro_rect = self.string.get_rect()
         self.intro_rect.x = self.x
+        self.k = 3
         self.intro_rect.top = self.y
         self.width = self.intro_rect.width
         self.height = self.intro_rect.height
 
     def draw(self):
         self.screen.blit(self.string, self.intro_rect)
-        pygame.draw.rect(self.screen, (255, 255, 255), (self.x, self.y, self.width, self.height), width=1)
+        pygame.draw.rect(self.screen, (255, 255, 255),
+                         (self.x - self.k, self.y - self.k, self.width + 2 * self.k, self.height + 2 * self.k),
+                         width=1)
 
     def checkMouse(self, mouse_pos):
-        x = mouse_pos.x
-        y = mouse_pos.y
-        if self.x <= x <= self.x + self.width and self.y <= y <= self.y + self.width:
-            return True
-        return False
+        x = mouse_pos[0]
+        y = mouse_pos[1]
+        if self.x - self.k <= x <= self.x + self.width + 2 * self.k and self.y - self.k <= y <= self.y \
+                + self.height + 2 * self.k:
+            self.func()
+        return
