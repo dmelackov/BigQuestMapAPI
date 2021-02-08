9cb9cdf63f5dc48882eae5793d14995b51269a6c
Ivanm <ivanm2114@mail.ru>
2021-02-08 17:52:53 +0300

    Button update 5 -button work properly finally

--- app/objects/pyGameButton.py
+++ app/objects/pyGameButton.py
@@ -2,11 +2,14 @@ import pygame
 
 
 class pyButton:
-    def __init__(self, x, y, text, screen,func,arr):
+    def __init__(self, x, y, text, screen, func, arr, toggle=False):
         arr.append(self)
         self.func = func
         self.x = x
         self.y = y
+        self.toggle = toggle
+        if self.toggle:
+            self.pressed = False
         self.text = text
         self.screen = screen
         self.font = pygame.font.Font(None, 30)
@@ -19,15 +22,37 @@ class pyButton:
         self.height = self.intro_rect.height
 
     def draw(self):
-        self.screen.blit(self.string, self.intro_rect)
-        pygame.draw.rect(self.screen, (255, 255, 255),
-                         (self.x - self.k, self.y - self.k, self.width + 2 * self.k, self.height + 2 * self.k),
-                         width=1)
+        if self.toggle:
+            if self.pressed:
+                pygame.draw.rect(self.screen, (255, 255, 255),
+                                 (self.x - self.k, self.y - self.k, self.width + 2 * self.k, self.height + 2 * self.k))
+                self.string = self.font.render(self.text, True, pygame.Color(0, 0, 0))
+                self.screen.blit(self.string, self.intro_rect)
+            else:
+                self.string = self.font.render(self.text, True, pygame.Color(255, 255, 255))
+                self.screen.blit(self.string, self.intro_rect)
+                pygame.draw.rect(self.screen, (255, 255, 255),
+                                 (self.x - self.k, self.y - self.k, self.width + 2 * self.k, self.height + 2 * self.k),
+                                 width=1)
+        else:
+            self.string = self.font.render(self.text, True, pygame.Color(255, 255, 255))
+            self.screen.blit(self.string, self.intro_rect)
+            pygame.draw.rect(self.screen, (255, 255, 255),
+                             (self.x - self.k, self.y - self.k, self.width + 2 * self.k, self.height + 2 * self.k),
+                             width=1)
 
     def checkMouse(self, mouse_pos):
-        x = mouse_pos[0]
-        y = mouse_pos[1]
-        if self.x - self.k <= x <= self.x + self.width + 2 * self.k and self.y - self.k <= y <= self.y \
-                + self.height + 2 * self.k:
-            self.func()
-        return
+        if self.toggle:
+            x = mouse_pos[0]
+            y = mouse_pos[1]
+            if self.x - self.k <= x <= self.x + self.width + 2 * self.k and self.y - self.k <= y <= self.y \
+                    + self.height + 2 * self.k:
+                self.pressed = not self.pressed
+                self.func(self.pressed)
+            return
+        else:
+            x = mouse_pos[0]
+            y = mouse_pos[1]
+            if self.x - self.k <= x <= self.x + self.width + 2 * self.k and self.y - self.k <= y <= self.y \
+                    + self.height + 2 * self.k:
+                self.func()
