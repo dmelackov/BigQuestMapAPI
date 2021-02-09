7519d7571b80d65ce10371a16f61b6d65a883f16
Ivanm <ivanm2114@mail.ru>
2021-02-09 14:35:39 +0300

    Switch update keyboard input added

--- app/objects/inputWindow.py
+++ app/objects/inputWindow.py
@@ -9,6 +9,7 @@ class inputWindow:
         self.height = height
         self.text = ''
         self.k = 3
+        self.i = 0
         self.active = False
         self.screen = screen
         self.fbuttons = [f'f{x}' for x in range(1, 13)]
@@ -18,13 +19,15 @@ class inputWindow:
         rect = string.get_rect()
         while rect.height < self.height:
             a += 1
-            font = pygame.font.Font(None, a)
+            font = pygame.font.SysFont('calibri', a)
             string = font.render('Test string', True, pygame.Color(255, 255, 255))
             rect = string.get_rect()
         a -= 1
-        string = font.render('s', True, pygame.Color(255, 255, 255))
+        self.font = pygame.font.SysFont('calibri', a)
+        string = font.render('D', True, pygame.Color(255, 255, 255))
         self.one_sym = string.get_rect().width
-        self.font = pygame.font.Font(None, a)
+        while self.width % self.one_sym:
+            self.width += 1
 
     def getText(self):
         return self.text
@@ -36,51 +39,38 @@ class inputWindow:
         else:
             self.active = False
 
-    def keyboardButtonPressed(self, button):
+    def keyboardButtonPressed(self, event):
+        button = event.unicode
         if self.active:
-            if button == 'backspace':
+            if event.key == pygame.K_BACKSPACE:
                 self.text = self.text[:-1]
-            elif button == 'space':
-                self.text += ' '
-            elif 'ctrl' in button or \
-                    'alt' in button or \
-                    'return' in button or\
-                    'delete' in button or\
-                    'print screen' in button or\
-                    'insert' in button or\
-                    'enter' in button or\
-                    'left' in button or\
-                    'up' in button or\
-                    'down' in button or\
-                    'right' in button or\
-                    'tab' in button or\
-                    'caps lock' in button or\
-                    'escape' in button or\
-                    'break' in button or\
-                    button in self.fbuttons:
-                pass
-            else:
+            elif button:
                 self.text += button
 
     def draw(self):
-        text = self.text
-        if len(self.text) > self.width // self.one_sym:
-            text = self.text[-self.width // self.one_sym + 1:]
+        x = self.x
+        y = self.y
+
+        f = -int(self.width * 0.075)
         if self.active:
             pygame.draw.rect(self.screen, (255, 255, 255),
                              (self.x - self.k, self.y - self.k, self.width + self.k * 2,
                               self.height + 2 * self.k))
-            string = self.font.render(text, True, pygame.Color(0, 0, 0))
-            rect = string.get_rect()
-            rect.x = self.x
-            rect.y = self.y
-            self.screen.blit(string, rect)
+            for sym in self.text[-self.width // self.one_sym - 8:]:
+                x += self.render(sym, (x, y))
+                if x > self.width:
+                    self.i += 1
         else:
             pygame.draw.rect(self.screen, (255, 255, 255),
                              (self.x - self.k, self.y - self.k, self.width + self.k * 2,
                               self.height + 2 * self.k), width=1)
-            string = self.font.render(text, True, pygame.Color(255, 255, 255))
-            rect = string.get_rect()
-            rect.x = self.x
-            rect.y = self.y
-            self.screen.blit(string, rect)
+            for sym in self.text[-self.width // self.one_sym - 8:]:
+                x += self.render(sym, (x, y), (255, 255, 255))
+
+    def render(self, sym, coords, color=(0, 0, 0)):
+        string = self.font.render(sym, True, pygame.Color(color))
+        rect = string.get_rect()
+        rect.x = coords[0]
+        rect.y = coords[1]
+        self.screen.blit(string, rect)
+        return rect.width
