03055dd3a03230b048ade958377fd1be1e447ec6
Ivanm <ivanm2114@mail.ru>
2021-02-11 17:12:37 +0300

    event given no mouse_pos

--- test Ivan.py
+++ test Ivan.py
@@ -21,13 +21,16 @@ down = pyButton(299, 625, '↓', screen, widgets, buttons, 605, 50, toggle=False
 right = pyButton(900, 174, '→', screen, widgets, buttons, 50, 455, toggle=False)
 left = pyButton(253, 174, '←', screen, widgets, buttons, 50, 455, toggle=False)
 plus_size = pyButton(1000, 174, '+', screen, widgets, buttons, 50, 50, toggle=False)
-minus_size = pyButton(1000, 224, '-', screen, widgets, buttons, 50,50, toggle=False)
+minus_size = pyButton(1000, 224, '-', screen, widgets, buttons, 50, 50, toggle=False)
 map = pyMap(300, 175, screen, widgets)
+
+
 up.setCheckKey(pygame.K_UP)
 down.setCheckKey(pygame.K_DOWN)
 left.setCheckKey(pygame.K_LEFT)
 right.setCheckKey(pygame.K_RIGHT)
-plus_size.setChek
+plus_size.setCheckKey(pygame.K_PAGEUP)
+minus_size.setCheckKey(pygame.K_PAGEDOWN)
 
 pygame.display.flip()
 clock = pygame.time.Clock()
