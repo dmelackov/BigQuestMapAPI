221b364cdc92188581c2500464b4a263b0fea634
Ivanm <ivanm2114@mail.ru>
2021-02-11 11:25:09 +0300

    interfce started

--- app/objects/pyGameButton.py
+++ app/objects/pyGameButton.py
@@ -2,13 +2,16 @@ import pygame
 
 
 class pyButton:
-    def __init__(self, x, y, text, screen, arr, buttons, toggle=False):
+    def __init__(self, x, y, text, screen, arr, buttons, width=None, height=None ,toggle=False):
         self.handler = None
         self.mouse = 0
         arr.append(self)
         buttons.append(self)
         self.x = x
+        self.width = width - 3
+        self.height = height - 3
         self.mode = 0
+        self.key = None
         self.y = y
         self.toggle = toggle
         if self.toggle:
@@ -17,12 +20,14 @@ class pyButton:
         self.screen = screen
         self.font = pygame.font.SysFont('calibri', 26)
         self.string = self.font.render(self.text, True, pygame.Color(255, 255, 255))
-        self.intro_rect = self.string.get_rect()
-        self.intro_rect.x = self.x
         self.k = 3
-        self.intro_rect.top = self.y
-        self.width = self.intro_rect.width
-        self.height = self.intro_rect.height
+        if not width:
+            self.width = self.intro_rect.width
+        if not height:
+            self.height = self.intro_rect.height
+        self.intro_rect = self.string.get_rect()
+        self.intro_rect.x = self.x + (self.width // 2) - (self.intro_rect.width // 2)
+        self.intro_rect.y = self.y + (self.height // 2) - (self.intro_rect.height // 2)
         self.timer = 100000
         self.anim = 0
 
@@ -41,30 +46,30 @@ class pyButton:
         self.timerCheck()
         if self.anim == 1:
             pygame.draw.rect(self.screen, (45, 45, 45),
-                             (self.x - self.k, self.y - self.k, self.width + 2 * self.k, self.height + 2 * self.k))
+                             (self.x, self.y, self.width + self.k, self.height + self.k))
             self.string = self.font.render(self.text, True, pygame.Color(0, 0, 0))
             self.screen.blit(self.string, self.intro_rect)
         elif self.toggle and self.mode and self.mouse:
             pygame.draw.rect(self.screen, (120, 120, 120),
-                             (self.x - self.k, self.y - self.k, self.width + 2 * self.k, self.height + 2 * self.k))
+                             (self.x, self.y, self.width + self.k, self.height + self.k))
             self.string = self.font.render(self.text, True, pygame.Color(0, 0, 0))
             self.screen.blit(self.string, self.intro_rect)
         elif self.mouse == 1:
             self.string = self.font.render(self.text, True, pygame.Color(120, 120, 120))
             self.screen.blit(self.string, self.intro_rect)
             pygame.draw.rect(self.screen, (120, 120, 120),
-                             (self.x - self.k, self.y - self.k, self.width + 2 * self.k, self.height + 2 * self.k),
+                             (self.x, self.y, self.width + self.k, self.height + self.k),
                              width=1)
         elif self.mode == 1:
             pygame.draw.rect(self.screen, (255, 255, 255),
-                             (self.x - self.k, self.y - self.k, self.width + 2 * self.k, self.height + 2 * self.k))
+                             (self.x, self.y, self.width + self.k, self.height + self.k))
             self.string = self.font.render(self.text, True, pygame.Color(0, 0, 0))
             self.screen.blit(self.string, self.intro_rect)
         else:
             self.string = self.font.render(self.text, True, pygame.Color(255, 255, 255))
             self.screen.blit(self.string, self.intro_rect)
             pygame.draw.rect(self.screen, (255, 255, 255),
-                             (self.x - self.k, self.y - self.k, self.width + 2 * self.k, self.height + 2 * self.k),
+                             (self.x, self.y, self.width + self.k, self.height + self.k),
                              width=1)
 
     def checkMouse(self, mouse_pos):
@@ -95,3 +100,6 @@ class pyButton:
             self.mouse = 1
         else:
             self.mouse = 0
+
+    def setCheckKey(self, key):
+        self.key = key
--- app/test Ivan.py
+++ app/test Ivan.py
@@ -10,7 +10,9 @@ buttons = []
 inputs = []
 font = pygame.font.SysFont('calibri', 26)
 
-
+up = pyButton(300, 176, '↑', screen, widgets, buttons, 600, 50, toggle=False)
+down = pyButton(300, 625, '↓', screen, widgets, buttons, 600, 50, toggle=False)
+left = pyButton(900, 101, '→', screen, widgets, buttons, 50, 400, toggle=False)
 
 pygame.display.flip()
 clock = pygame.time.Clock()
@@ -29,7 +31,7 @@ while True:
     [x.mouseonButton(pygame.mouse.get_pos()) for x in buttons]
     [x.draw() for x in widgets]
     pygame.draw.rect(screen, (255, 255, 255),
-                     (300, 100, 600, 600), width=1)
+                     (300, 175, 600, 450), width=1)
     string = font.render('Место для карты', True, pygame.Color(255, 255, 255))
     screen.blit(string, (500, 380, 200, 50))
     pygame.display.flip()
